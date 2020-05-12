4407fbaad8616c09145c6bb3c5c62e4fbdffbacd
sahithi0511 <63602952+sahithi0511@users.noreply.github.com>
2020-05-12 07:25:28 +0530

    02 intermediate programs

---
+++ myObjects.js
@@ -0,0 +1,20 @@
+let myYoutubeVideo = {
+    title:'Web',
+    videoLength:15,
+    videoCreator:'Sahi',
+    videoDescription:'this is video description and a lng one'
+}
+console.log(myYoutubeVideo);
+console.log(`Hey new video by ${myYoutubeVideo.videoCreator} on ${myYoutubeVideo.title}`);
+
+
+//Assignment
+//name,price,description
+
+let Course =  {
+ name:'Javascript',
+ Price: 1000,
+ description:'This course is based on the javascript basics to advance'
+}
+console.log(`This course is about ${Course.name} getting in a low price of ${Course.Price} and ${Course.description}`);
+
