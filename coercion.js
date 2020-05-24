3762918cd1230d6d673443cca68e1b52e8e3ec79
sahithi0511 <63602952+sahithi0511@users.noreply.github.com>
2020-05-24 13:02:44 +0530

    01 advance

---
+++ coercion.js
@@ -0,0 +1,23 @@
+console.log('20' - 5);
+// const giveType = typeof {}
+// console.log(giveType);
+
+// const adder = 5 + true
+// console.log(adder);
+
+const loginDetails = []
+//Login for getting details from db
+const loginId = loginDetails[0]
+if (loginId) {
+    console.log('Allow user to login')
+}
+else{
+    console.log('Auth failed')
+}
+//Values that interpret as false
+//false
+//0
+//''
+//null
+//undefined
+
