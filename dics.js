b3d56e4b1c975b0e27bf4ffb4842d187869907af
sahithi0511 <63602952+sahithi0511@users.noreply.github.com>
2020-05-24 09:59:43 +0530

    02 intermediate

---
+++ dics.js
@@ -0,0 +1,12 @@
+// let numOne = 22
+// let numTwo = 7
+// let res = numOne /numTwo
+// console.log(res.toFixed(2));
+// let floatRes = res.toFixed(2)
+// console.log(Math.floor(floatRes));
+// console.log((Math.ceil(floatRes)));
+// console.log(Math.random() * 5 + 1);
+let upper = 6
+let lower = 1
+let myRandom = Math.ceil(Math.random() * (upper - lower + 1)) 
+console.log(myRandom);
